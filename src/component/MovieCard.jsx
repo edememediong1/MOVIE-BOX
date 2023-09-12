c1f44fb260c0db4efdba587e97f94d16cea7cd2a
Emediong Bassey Edem <edememediong1@gmail.com>
2023-09-12 14:17:20 +0100

    added prop validation to the poster_path

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -2,16 +2,20 @@
 import PropTypes from 'prop-types';
 
 function MovieCard({movie}){
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/w500/"
     return (
-        <div>
-            {movie.title}
-        </div>
-    )
-}
+          <div data-testid="movie-card" id="movie-card">
+              {movie.poster_path ? <img src= {`${IMAGE_PATH}${movie.poster_path}`} data-testid="movie-poster" alt='movie-poster'/>
+                : null}
+              <p data-testid="movie-title">{movie.title}</p>
+
+          </div> )
+      }
 
 MovieCard.propTypes = {
     movie: PropTypes.shape({
-      title: PropTypes.string.isRequired, // Define title as a required string prop
+      title: PropTypes.string.isRequired,
+       poster_path: PropTypes.img.isRequired// Define title as a required string prop
       // Other properties of the 'movie' object and their prop validations
     }).isRequired,
   };

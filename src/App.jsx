878dc40e9ff1ca4eb1cc159870f405f09e28677d
Emediong Bassey Edem <edememediong1@gmail.com>
2023-09-14 00:40:22 +0100

    added background image to the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,21 @@ import MovieCard from './component/MovieCard';
 function App() {
   const API_URL = "https://api.themoviedb.org/3/"
   const [movies, setMovies] = useState([])
+  const [searchKey, setSearchKey]= useState([])
+  const [selectMovie, setSelectMovie] = useState({})
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/original/"
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (searchKey) => {
+   
     try {
-      const {data:{results}} = await axios.get(`${API_URL}/movie/top_rated`, {
+      const {data:{results}} = await axios.get(`${API_URL}/${searchKey ? "search/movie" : "discover/movie"}`, {
         params: {
-          api_key: import.meta.env.VITE_REACT_APP_MOVIE_API_KEY
-        }
-      })    
+          api_key: import.meta.env.VITE_REACT_APP_MOVIE_API_KEY,
+          query: searchKey
+        } 
+      }) 
     setMovies(results)
+    setSelectMovie(results[0])
     }
     catch(error){
       console.log("There's an error", error)
@@ -32,10 +38,33 @@ function App() {
     />)
   })
   
-
+const searchMovies = (e) => {
+  e.preventDefault()
+  fetchMovies(searchKey)
+}
   return (
-    <div>
-      <div>Hello World, I am here </div>
+    <div className='App'>
+      <header className="header" style={{backgroundImage: `url('${IMAGE_PATH}${selectMovie.backdrop_path}')`}}>
+        <div className='header-content'>
+          <img src='./Logo.svg' />
+          <form onSubmit={searchMovies}>
+              <input type="text" onChange={(e => setSearchKey(e.target.value))}/>
+              <button type='submit'> <i className="fa fa-search"></i></button>
+          </form>
+        </div>
+        <div className='Hero'>
+          <h1>{selectMovie.title}</h1>
+          <div className='rating'>
+            <span><img src="./src/assets/imdb.svg"/><p>{selectMovie.vote_average}/10</p></span>
+            <span><img src="./src/assets/rot.svg"/><p>97%</p></span>
+          </div>
+          {selectMovie.overview ? <p>{selectMovie.overview}</p> : null}
+          <button className='trailer-button'><img src="./src/assets/button.svg"/></button>
+        </div>
+
+      </header>
+      
+      <h1>Featured Movies</h1>
       <div className='container'>
         {movies.length > 0 ? renderMovies : <h1>Loading</h1>}
       </div>
--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -5,8 +5,8 @@ function MovieCard({movie}){
   const IMAGE_PATH = "https://image.tmdb.org/t/p/w500/"
     return (
           <div data-testid="movie-card" id="movie-card">
-              {movie.poster_path ? <img src= {`${IMAGE_PATH}${movie.poster_path}`} data-testid="movie-poster" alt='movie-poster'/>
-                : null}
+              {movie.poster_path ? <img src= {`${IMAGE_PATH}${movie.poster_path}`} data-testid="movie-poster" alt='movie-poster' width={250} height={370}/>
+                : <div className='placeholder'> No image Found</div> }
               <p data-testid="movie-title">{movie.title}</p>
 
           </div> )
@@ -15,7 +15,7 @@ function MovieCard({movie}){
 MovieCard.propTypes = {
     movie: PropTypes.shape({
       title: PropTypes.string.isRequired,
-       poster_path: PropTypes.img.isRequired// Define title as a required string prop
+       poster_path: PropTypes.img// Define title as a required string prop
       // Other properties of the 'movie' object and their prop validations
     }).isRequired,
   };
